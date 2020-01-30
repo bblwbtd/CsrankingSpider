41169c825745dd6b71b463d64869cc734f7b4a05
bblwbtd <gmyxgmyx@outlook.com>
2020-01-30 20:38:23 +0800

    Automatically create output directory.

--- Startup.py
+++ Startup.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 
 from src.CsrankingSpider.main import main
 
@@ -7,6 +8,8 @@ batch_size = 10
 csranking_csv_file = 'csranking.csv'
 
 if __name__ == '__main__':
+    if os.path.exists(output_directory) is False:
+        os.mkdir(output_directory)
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main(output_directory, batch_size, csranking_csv_file))
     loop.run_until_complete(asyncio.sleep(0.250))
